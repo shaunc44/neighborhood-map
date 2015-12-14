92510c2dbdd0170f95238ec5b57438d39101db1b
Shaun <seyyes2000@hotmail.com>
2015-12-14 14:19:16 -0500

    Fix infowindows

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ var locations = [
 		name: "Switchback Brewing",
 		lat: 44.456184,
 		lng: -73.220686,
-		id: "44be993be62c0c928f7cddfd4"
+		id: "4be993be62c0c928f7cddfd4"
 	},
 	{
 		name: "Citizen Cider",
@@ -47,29 +47,36 @@ var locations = [
 		lat: 44.475682,
 		lng: -73.2131557,
 		id: "4bc1bca8920eb7135f151b2c"
+	},
+	{
+		name: "American Flatbread",
+		lat: 44.4765430,
+		lng: -73.2142900,
+		id: "4af3a181f964a520fcee21e3"
 	}
 ];
+
 
 //Initialize the map
 var map;
 function initMap() {
+	"use strict";
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 44.47125, lng: -73.2125},
 		zoom: 14
 	});
-ko.applyBindings(new ViewModel());
-}
 
+	ko.applyBindings(new ViewModel());
+}
 
 // Alert the user if google maps isn't working
 function googleError() {
 	"use strict";
-	document.getElementById('map').innerHTML = "<h2>Google Maps is not loading</h2>";
-};
-
+	document.getElementById('map').innerHTML = "<h2>Google Maps is not loading. Please refresh the page later.</h2>";
+}
 
 // Place constructor
-var Place = function(data) {
+var Place = function (data) {
 	"use strict";
 	this.name = ko.observable(data.name);
 	this.lat = ko.observable(data.lat);
@@ -87,60 +94,62 @@ var Place = function(data) {
 	this.contentString = ko.observable('');
 };
 
+
 // ViewModel
-var ViewModel = function() {
+var ViewModel = function () {
+	"use strict";
 	var self = this;
 
-	// Create an array of all places
+	//Create an array of all places
 	this.placeList = ko.observableArray([]);
 
-	// Create Place objects for each item in locations & store them in the above array
-	locations.forEach(function (placeItem) {
+	//Create Place objects for each item in locations & store them in the above array
+	locations.forEach(function(placeItem) {
 		self.placeList.push(new Place(placeItem));
 	});
 
-	// Initialize the infowindow
-	var infowindow = new google.maps.InfoWindow();
+	//Initialize the infowindow
+	var infowindow = new google.maps.InfoWindow({
+		maxWidth: 200
+	});
 
 	// Initialize marker
 	var marker;
 
 	self.placeList().forEach(function (placeItem) {
-
 		// Define markers for each place
 		marker = new google.maps.Marker({
 			position: new google.maps.LatLng(placeItem.lat(),placeItem.lng()),
 			map: map,
-			animation: google.maps.Animation.DROP,
-			title: placeItem.name()
+			animation: google.maps.Animation.DROP
 		});
 		placeItem.marker = marker;
 
-
-		// Make AJAX request to Foursquare
+		// Make AJAX requests to Foursquare
 		$.ajax({
-			url: 'https://api.foursquare.com/v2/venues/search' + placeItem.id() + '?client_id=OS1S4RTHIBE0UB0IZXWOMTNBTKR0OORIGMBHTKOMMLG5DVSY&client_secret=KVDZNXLVF2HSZCWLRXOA0SZ4HCYIW2NCBTYATX2ZYYHRHShFA&v=20130815&ll=',
+			url: 'https://api.foursquare.com/v2/venues/' + placeItem.id() + '?client_id=OS1S4RTHIBE0UB0IZXWOMTNBTKR0OORIGMBHTKOMMLG5DVSY&client_secret=KVDZNXLVF2HSZCWLRXOA0SZ4HCYIW2NCBTYATX2ZYYHRHSFA&v=20130815',
 			dataType: "json",
 
-			success: function(data) {
+			success: function (data) {
 				var result = data.response.venue;
-				console.dir(placeItem.name());
-				console.dir(result);
-				placeItem.name(result.name);
 
 				var contact = result.hasOwnProperty('contact') ? result.contact : '';
 				if (contact.hasOwnProperty('formattedPhone')) {
-				    placeItem.phone(contact.formattedPhone || '');
+					placeItem.phone(contact.formattedPhone || '');
 				}
 
 				var location = result.hasOwnProperty('location') ? result.location : '';
 				if (location.hasOwnProperty('address')) {
-				    placeItem.address(location.address || '');
+					placeItem.address(location.address || '');
 				}
 
 				var bestPhoto = result.hasOwnProperty('bestPhoto') ? result.bestPhoto : '';
+				if (bestPhoto.hasOwnProperty('prefix')) {
+					placeItem.photoPrefix(bestPhoto.prefix || '');
+				}
+
 				if (bestPhoto.hasOwnProperty('suffix')) {
-				    placeItem.photoSuffix(bestPhoto.suffix || '');
+					placeItem.photoSuffix(bestPhoto.suffix || '');
 				}
 
 				var description = result.hasOwnProperty('description') ? result.description : '';
@@ -153,66 +162,96 @@ var ViewModel = function() {
 				placeItem.url(url || '');
 
 				placeItem.canonicalUrl(result.canonicalUrl);
-			},
 
-			// this cause the map to go blank??????
-			error: function(e) {
-				infowindow.setContent('<h5>Foursquare data is unavailable. Please try refreshing later.</h5>')
+				//Infowindow code is in the success function so that the error message displayed in infowindow works properly
+				//Content of the infowindow
+				var contentString = '<div id="iWindow"><h4>' + placeItem.name() + '</h4><div id="pic"><img src="' +
+					placeItem.photoPrefix() + '110x110' + placeItem.photoSuffix() +
+					'" alt="Image Location"></div><p>Information from Foursquare:</p><p>' +
+					placeItem.phone() + '</p><p>' + placeItem.address() + '</p><p>' +
+					placeItem.description() + '</p><p>Rating: ' + placeItem.rating() +
+					'</p><p><a href=' + placeItem.url() + '>' + placeItem.url() +
+					'</a></p><p><a target="_blank" href=' + placeItem.canonicalUrl() +
+					'>Foursquare Page</a></p><p><a target="_blank" href=https://www.google.com/maps/dir/Current+Location/' +
+					placeItem.lat() + ',' + placeItem.lng() + '>Directions</a></p></div>';
+
+				// Add infowindows credit http://you.arenot.me/2010/06/29/google-maps-api-v3-0-multiple-markers-multiple-infowindows/
+				google.maps.event.addListener(placeItem.marker, 'click', function() {
+						infowindow.open(map, this);
+						placeItem.marker.setAnimation(google.maps.Animation.BOUNCE);
+						setTimeout(function () {
+							placeItem.marker.setAnimation(null);
+						}, 500);
+						infowindow.setContent(contentString);
+				});
+			},
+			// Alert the user on error. Set messages in the DOM and infowindow
+			error: function (e) {
+				infowindow.setContent('<h5>Foursquare data is unavailable. Please try refreshing later.</h5>');
+				document.getElementById("error").innerHTML = "<h4>Foursquare data is unavailable. Please try refreshing later.</h4>";
 			}
 		});
 
-
-		function toggleBounce() {
-			if(placeItem.marker.getAnimation() !== null) {
-				placeItem.marker.setAnimation(null);
-			} else {
-				placeItem.marker.setAnimation(google.maps.Animation.BOUNCE);
-			}
-		}
-
-		//Add infowindows
+		//Event listener makes the error message on AJAX error display in the infowindow
 		google.maps.event.addListener(marker, 'click', function () {
 			infowindow.open(map, this);
-			toggleBounce();
-			setTimeout(toggleBounce, 500);
-
-		//getFoursquare();
-			infowindow.setContent('<h4>' + placeItem.name() + '</h4><img src="' + placeItem.photoPrefix() + '110x110' + placeItem.photoSuffix() + '" alt="Image Location"><p>Information from Foursquare:</p><p>' + placeItem.phone() + '</p><p>' + placeItem.address() + '</p><p>' + placeItem.description() + '</p><p>Rating: '+ placeItem.rating() + '</p><p><a href=' + placeItem.url() + '>' + placeItem.url() + '</a></p><p><a target="_blank" href=' + placeItem.canonicalUrl() + '>Foursquare Page</a></p><p><a target="_blank" href=https://www.google.com/maps/dir/Current+Location/' + placeItem.lat() + ',' + placeItem.lng() + '>Directions</a></p>')
+			placeItem.marker.setAnimation(google.maps.Animation.BOUNCE);
+			setTimeout(function () {
+				placeItem.marker.setAnimation(null);
+			}, 500);
 		});
 	});
 
-	self.showInfo = function(placeItem) {
+	// Activate the appropriate marker when the user clicks a list item
+	self.showInfo = function (placeItem) {
 		google.maps.event.trigger(placeItem.marker, 'click');
+		self.hideElements();
 	};
 
+	// Toggle the nav class based style
+	self.toggleNav = ko.observable(false);
+	this.navStatus = ko.pureComputed (function () {
+		return self.toggleNav() === false ? 'nav' : 'navClosed';
+		}, this);
+
+	self.hideElements = function (toggleNav) {
+		self.toggleNav(true);
+		return true;
+	};
+
+	self.showElements = function (toggleNav) {
+		self.toggleNav(false);
+		return true;
+	};
+
+	// Filter markers per user input
 	// Array containing only the markers based on search
 	self.visible = ko.observableArray();
 
-	// All markers are visible by default before any user search
-	self.placeList().forEach(function(place) {
+	// All markers are visible by default before any user input
+	self.placeList().forEach(function (place) {
 		self.visible.push(place);
 	});
 
-	// Keep track of user input
+	// Track user input
 	self.userInput = ko.observable('');
-
-	// Filter markers: Set all markers to not visible. 
-	// Only display them if they match user search input
-	// Credit http://codepen.io/prather-mcs/pen/KpjbNN?editors=001
-	self.filterMarkers = function() {
+	// If user input is included in the place name, make it and its marker visible
+	// Otherwise, remove the place & marker
+	self.filterMarkers = function () {
+		// Set all markers and places to not visible.
 		var searchInput = self.userInput().toLowerCase();
 		self.visible.removeAll();
-		// Compare the name of each place to the user input
-		// If it matches, display the place and marker
-		self.placeList().forEach(function(place) {
+		self.placeList().forEach(function (place) {
 			place.marker.setVisible(false);
-		if (place.name().toLowerCase().indexOf(searchInput) !== -1) {
-			self.visible.push(place);
+			// Compare the name of each place to user input
+			// If user input is included in the name, set the place and marker as visible
+			if (place.name().toLowerCase().indexOf(searchInput) !== -1) {
+				self.visible.push(place);
 			}
 		});
-		self.visible().forEach(function(place){
+		self.visible().forEach(function (place) {
 			place.marker.setVisible(true);
-		})
+		});
 	};
 
-}//ViewModel
+}; // ViewModel
