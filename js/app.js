70447a203716f7e1202871fa6a79f79fc076d362
Shaun <seyyes2000@hotmail.com>
2015-12-14 16:03:45 -0500

    Add mouseover to infowindow; Change pageTitle font

--- js/app.js
+++ js/app.js
@@ -1,34 +1,28 @@
 //Locations' Array
 var locations = [
 	{
+		name: "American Flatbread",
+		lat: 44.4765430,
+		lng: -73.2142900,
+		id: "4af3a181f964a520fcee21e3"
+	},
+	{
 		name: "Church Street Marketplace",
 		lat: 44.4781653,
 		lng: -73.2126255,
 		id: "4a6ca420f964a5201ad11fe3"
 	},
 	{
-		name: "University of Vermont",
-		lat: 44.4778528, 
-		lng: -73.1964637,
-		id: "4c2b717857a9c9b670e3f567"
-	},
-	{
-		name: "Waterfront Park",
-		lat: 44.4800847,
-		lng: -73.2214102,
-		id: "4bb8e3fa98c7ef3b058a3102"
-	},
-	{
-		name: "Switchback Brewing",
-		lat: 44.456184,
-		lng: -73.220686,
-		id: "4be993be62c0c928f7cddfd4"
-	},
-	{
 		name: "Citizen Cider",
 		lat: 44.4706803,
 		lng: -73.214242,
 		id: "533f48d3498eb76534dffbb2"
+	},
+	{
+		name: "Flynn Center for the Performing Arts",
+		lat: 44.475682,
+		lng: -73.2131557,
+		id: "4bc1bca8920eb7135f151b2c"
 	},
 	{
 		name: "Hen of the Wood",
@@ -43,21 +37,27 @@ var locations = [
 		id: "4b4cbd1ef964a520a4bc26e3"
 	},
 	{
-		name: "Flynn Center for the Performing Arts",
-		lat: 44.475682,
-		lng: -73.2131557,
-		id: "4bc1bca8920eb7135f151b2c"
+		name: "Switchback Brewing",
+		lat: 44.456184,
+		lng: -73.220686,
+		id: "4be993be62c0c928f7cddfd4"
 	},
 	{
-		name: "American Flatbread",
-		lat: 44.4765430,
-		lng: -73.2142900,
-		id: "4af3a181f964a520fcee21e3"
+		name: "University of Vermont",
+		lat: 44.4778528, 
+		lng: -73.1964637,
+		id: "4c2b717857a9c9b670e3f567"
+	},
+	{
+		name: "Waterfront Park",
+		lat: 44.4800847,
+		lng: -73.2214102,
+		id: "4bb8e3fa98c7ef3b058a3102"
 	}
 ];
 
 
-//Initialize the map
+//Initialize map of Burlington, VT
 var map;
 function initMap() {
 	"use strict";
@@ -69,13 +69,13 @@ function initMap() {
 	ko.applyBindings(new ViewModel());
 }
 
-// Alert the user if google maps isn't working
+//Alert user when google maps isn't working
 function googleError() {
 	"use strict";
 	document.getElementById('map').innerHTML = "<h2>Google Maps is not loading. Please refresh the page later.</h2>";
 }
 
-// Place constructor
+//Place constructor
 var Place = function (data) {
 	"use strict";
 	this.name = ko.observable(data.name);
@@ -103,19 +103,18 @@ var ViewModel = function () {
 	//Create an array of all places
 	this.placeList = ko.observableArray([]);
 
-	//Create Place objects for each item in locations & store them in the above array
+	//Create Place objects for each location item & store in the above array
 	locations.forEach(function(placeItem) {
 		self.placeList.push(new Place(placeItem));
 	});
 
-	//Initialize the infowindow
+	//Initialize the Infowindow
 	var infowindow = new google.maps.InfoWindow({
 		maxWidth: 200
 	});
 
-	// Initialize marker
+	//Initialize markers
 	var marker;
-
 	self.placeList().forEach(function (placeItem) {
 		// Define markers for each place
 		marker = new google.maps.Marker({
@@ -163,8 +162,7 @@ var ViewModel = function () {
 
 				placeItem.canonicalUrl(result.canonicalUrl);
 
-				//Infowindow code is in the success function so that the error message displayed in infowindow works properly
-				//Content of the infowindow
+				//Content of the Infowindow
 				var contentString = '<div id="iWindow"><h4>' + placeItem.name() + '</h4><div id="pic"><img src="' +
 					placeItem.photoPrefix() + '110x110' + placeItem.photoSuffix() +
 					'" alt="Image Location"></div><p>Information from Foursquare:</p><p>' +
@@ -175,8 +173,8 @@ var ViewModel = function () {
 					'>Foursquare Page</a></p><p><a target="_blank" href=https://www.google.com/maps/dir/Current+Location/' +
 					placeItem.lat() + ',' + placeItem.lng() + '>Directions</a></p></div>';
 
-				// Add infowindows credit http://you.arenot.me/2010/06/29/google-maps-api-v3-0-multiple-markers-multiple-infowindows/
-				google.maps.event.addListener(placeItem.marker, 'click', function() {
+				//Infowindow that opens upon mouseover
+				google.maps.event.addListener(placeItem.marker, 'mouseover', function() {
 						infowindow.open(map, this);
 						placeItem.marker.setAnimation(google.maps.Animation.BOUNCE);
 						setTimeout(function () {
@@ -185,15 +183,16 @@ var ViewModel = function () {
 						infowindow.setContent(contentString);
 				});
 			},
-			// Alert the user on error. Set messages in the DOM and infowindow
+
+			//Foursquare error
 			error: function (e) {
 				infowindow.setContent('<h5>Foursquare data is unavailable. Please try refreshing later.</h5>');
 				document.getElementById("error").innerHTML = "<h4>Foursquare data is unavailable. Please try refreshing later.</h4>";
 			}
 		});
 
-		//Event listener makes the error message on AJAX error display in the infowindow
-		google.maps.event.addListener(marker, 'click', function () {
+		//Event listener shows error message on AJAX error display in the infowindow
+		google.maps.event.addListener(marker, 'mouseover', function () {
 			infowindow.open(map, this);
 			placeItem.marker.setAnimation(google.maps.Animation.BOUNCE);
 			setTimeout(function () {
@@ -202,13 +201,13 @@ var ViewModel = function () {
 		});
 	});
 
-	// Activate the appropriate marker when the user clicks a list item
+	//Activate the appropriate marker when the user mouseovers a list item
 	self.showInfo = function (placeItem) {
-		google.maps.event.trigger(placeItem.marker, 'click');
+		google.maps.event.trigger(placeItem.marker, 'mouseover');
 		self.hideElements();
 	};
 
-	// Toggle the nav class based style
+	//Toggle the nav class based style
 	self.toggleNav = ko.observable(false);
 	this.navStatus = ko.pureComputed (function () {
 		return self.toggleNav() === false ? 'nav' : 'navClosed';
@@ -224,27 +223,28 @@ var ViewModel = function () {
 		return true;
 	};
 
-	// Filter markers per user input
-	// Array containing only the markers based on search
+	//Filter markers per user input
+	//Array reveals only visible markers based on search
 	self.visible = ko.observableArray();
 
-	// All markers are visible by default before any user input
+	//All markers are visible by default before any user input
 	self.placeList().forEach(function (place) {
 		self.visible.push(place);
 	});
 
-	// Track user input
+	//Track user input
 	self.userInput = ko.observable('');
-	// If user input is included in the place name, make it and its marker visible
-	// Otherwise, remove the place & marker
+
+	//If user input is included in the place name make visible, otherwise, remove the place & marker
 	self.filterMarkers = function () {
-		// Set all markers and places to not visible.
+
+		//Set all markers and places to not visible.
 		var searchInput = self.userInput().toLowerCase();
 		self.visible.removeAll();
 		self.placeList().forEach(function (place) {
 			place.marker.setVisible(false);
-			// Compare the name of each place to user input
-			// If user input is included in the name, set the place and marker as visible
+
+			//If user input is part of list set the place and marker as visible
 			if (place.name().toLowerCase().indexOf(searchInput) !== -1) {
 				self.visible.push(place);
 			}
@@ -254,4 +254,4 @@ var ViewModel = function () {
 		});
 	};
 
-}; // ViewModel
+};//ViewModel
