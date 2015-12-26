fc7e407838da02a1d4e9cec7a1bb7f9ae94858b5
Shaun <seyyes2000@hotmail.com>
2015-12-25 21:21:22 -0500

    Add changes to expandable search box - still not working :(

--- js/searchbox.js
+++ js/searchbox.js
@@ -1,21 +1,26 @@
 // Credit: http://codepen.io/nikhil/pen/qcyGF
 
+//Use ready() to make a function available after the document is loaded
 $(document).ready(function(){
     var submitIcon = $('.searchbox-icon');
     var inputBox = $('.searchbox-input');
     var searchBox = $('.searchbox');
     var isOpen = false;
+    //Run function when submitIcon is clicked
     submitIcon.click(function(){
-        if(isOpen == false){
+        if (isOpen == false){
+            //Add a class name to the first p element of searchbox
             searchBox.addClass('searchbox-open');
             inputBox.focus();
             isOpen = true;
         } else {
+            //Removes searchbox-open class from all p elements
             searchBox.removeClass('searchbox-open');
             inputBox.focusout();
             isOpen = false;
         }
-    });  
+    });
+    //Release the mouse button over submit icon and searchbox???
     submitIcon.mouseup(function(){
             return false;
     });
@@ -23,7 +28,7 @@ $(document).ready(function(){
             return false;
     });
     $(document).mouseup(function(){
-        if(isOpen == true){
+        if (isOpen == true){
             $('.searchbox-icon').css('display','block');
             submitIcon.click();
         }
@@ -31,9 +36,11 @@ $(document).ready(function(){
 });
 
 function buttonUp(){
+    //Return the value of searchbox-input
     var inputVal = $('.searchbox-input').val();
+    //.trim() removes whitespace from both sides of the string
     inputVal = $.trim(inputVal).length;
-    if(inputVal !== 0){
+    if (inputVal !== 0){
         $('.searchbox-icon').css('display','none');
     } else {
         $('.searchbox-input').val('');
