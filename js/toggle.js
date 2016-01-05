071c1a41f83d01068cc8c407435dc9ea5a8e79d5
Shaun <seyyes2000@hotmail.com>
2016-01-05 17:34:01 -0500

    Remove some unnecessary code

--- js/toggle.js
+++ js/toggle.js
@@ -3,24 +3,8 @@
 $(function() {
     //run the currently selected effect
     function runEffect() {
-        //get effect type from
-        var selectedEffect = $("#effectTypes").val();
-
         //most effect types need no options passed by default
         var options = {};
-        //some effects have required parameters
-        if (selectedEffect === "slide") {
-            options = {
-                percent: 0
-            };
-        } else if (selectedEffect === "size") {
-            options = {
-                to: {
-                    width: 200,
-                    height: 60
-                }
-            };
-        }
 
         //run the effect
         //$("#effect").toggle(selectedEffect, options, 1000);
